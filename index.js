4e9f0cdf26fcb39e2e29e67d809c83f1533b245a
Paul Freund <freund.paul@lvl3.org>
2021-01-28 17:09:36 +0100

    Added writeHead and made processData an async function

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const http = require("http");
 const workers = require('./workers');
 
-const srv = http.createServer(function(req, res) {
-  const begin = new Date();
+async function processData(event) {
+    workers.forEach(function(worker) {
+        worker({
+            event
+        });
+    });
+}
 
-  if (req.method !== 'POST') {
-    res.statusCode = 400;
+const srv = http.createServer(function (req, res) {
+    if(req.method == 'POST') {
+        var body = '';
+ 
+        req.on('data', (data) => {
+            body += data
+        });
+ 
+        req.on('end', () => {
+            for(let event of body.split("\n")) {
+                if(event.trim().length > 0) {
+                    processData(event)
+                }
+            }
+        });
+    }
+
+    res.writeHead(200);
     res.end();
 
-    return;
-  }
-
-  let body = '';
-
-  req.on('data', (data) => {
-    body += data
-  });
-
-  req.on('end', () => {
-    workers.forEach(function(worker) {
-      worker({
-        body
-      });
-    });
-  });
-
-  res.statusCode = 200;
-  res.end();
-
-  console.log(`Time: ${ new Date() - begin }`);
 }).listen(80, () => {
-  console.log('TestCollector started on port ' + srv.address().port);
+    console.log('TestCollector started on port ' + srv.address().port);
 });
